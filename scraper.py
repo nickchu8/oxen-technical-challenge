002a124336c31af7a72a83a6366fcf63ed312b70
Nick Chu <nyc8pv@gmail.com>
2023-11-15 16:10:41 -0800

    final changes to files

--- scraper.py
+++ scraper.py
@@ -1,12 +1,6 @@
 import praw
 import pandas as pd
 from datetime import datetime
-import oxen
-from oxen.auth import config_auth
-from oxen.user import config_user
-import os
-from oxen.remote_repo import create_repo
-from oxen import LocalRepo
 
 
 # Use praw, reddit api to extract info and create a reddit instance
@@ -39,37 +33,3 @@ for post in hot_posts:
 df = pd.DataFrame(post_list) #create dataframe from our list
 csv_path = "oxennbareddit.csv" 
 df.to_csv(csv_path, index=False, encoding='utf-8') #create csv!
-
-# #now that we have the csv, let's upload it to oxen
-# config_auth("SFMyNTY.g2gDbQAAAC9hcGlfa2V5X3YxOjZhMzc3ZDgwLWIzNDktNGJiMy05MTRjLTJlMWRiYzcyMzExOW4GAB6LNtWLAWIAAVGA.Uv6DnRrz5XM8XNdYXpFXQoymIcrIzBc4omLzFknYCho")
-# config_user("Nicholas Chu", "nyc8pv@gmail.com")
-
-# # Create an empty directory named redditchallenge
-# directory = "redditchallenge"
-# os.makedirs(directory)
-
-# # Initialize the Oxen Repository
-# repo = oxen.init(directory)
-
-# # Create a README.md file
-# filename = os.path.join(repo.path, "README.md")
-# with open(filename, "w") as f:
-#     f.write("# Scraping r/nba to get 1000 hot posts")
-# # Commit the changes to the repository
-# repo.commit("Adding README.md")
-
-# # Create a remote repository
-# remote_name = "nickchu8/redditchallenge"
-# create_repo(remote_name)
-
-# # Load the local repository
-# local_repo = LocalRepo("redditchallenge")
-
-# # Point the local repository to the remote
-# local_repo.set_remote(remote_name)
-
-# # Push the changes to the remote repository
-# local_repo.push()
-
-# # Add the README.md file to the staging area
-# repo.add(filename)
